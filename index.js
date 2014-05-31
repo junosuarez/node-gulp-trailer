28a1ebe36fd8c0291fe2fabac1a4b0fc8f4de94f
jden <jason@denizac.org>
2014-05-31 04:13:27 -0400

    pasting right things in right files helps

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-function gulpTrailer () {
-  
+var through = require('through2')
+var File = require('vinyl')
+
+// attempt to send another obj in the stream at the end
+function trailer(file, fn) {
+  return through({objectMode: true}, function (a, e, cb) {
+    this.push(a)
+    cb()
+  }, function (cb) {
+    var self = this
+    fn(function (err, val) {
+      if (err) { return cb(err) }
+      if (val) {
+
+        if (typeof val === 'string') {
+          val = new Buffer(string)
+        }
+        
+        var f = new File({
+          path: file,
+          contents: val
+        })
+
+        self.push(f)
+      }
+      cb()  
+    })
+    
+  })
 }
 
-module.exports = gulpTrailer
+module.exports = trailer
